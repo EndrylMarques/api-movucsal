e53038d7bebb01908327ea64ec0107bdf6053e23
Endryl Santos <esantos@redventures.com>
2020-11-02 19:23:33 -0300

    Adicionando projeto base spring boot

---
+++ src/main/java/br/com/milleddy/muvucsal/MuvucsalApplication.java
@@ -0,0 +1,13 @@
+package br.com.milleddy.muvucsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuvucsalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuvucsalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/milleddy/muvucsal/MuvucsalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.milleddy.muvucsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuvucsalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
